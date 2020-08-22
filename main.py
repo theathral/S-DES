361fc52ec35060a7c9d4ecc4a6ce048055ec7f8f
theodosisathanasakis <th.athanasakis@gmail.com>
2020-08-22 17:23:34 +0300

    basic algorithm structure to do: s-boxes algorithm

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import sdes_encryption
 
 # define variables
 def_key = [1, 0, 1, 0, 0, 0, 0, 0, 1, 0]  # 10-bit [0, 1]
+
+ip = [1, 5, 2, 0, 3, 7, 4, 6]
+ip_inv = [3, 0, 2, 4, 6, 1, 7, 5]
+ep = []
+s0 = []
+s1 = []
 p10 = [2, 4, 1, 6, 3, 9, 0, 8, 7, 5]  # 10-bit [0, 9]
 p8 = [5, 2, 6, 3, 7, 4, 9, 8]  # 8-bit [0, 9]
+p4 = []
 
 
 # key generation #
@@ -17,4 +24,4 @@ def key_gen(n):
 
 
 plain_text = []
-sdes_encryption.encryption(plain_text, def_key, p10, p8)
+sdes_encryption.encryption(plain_text, def_key, ip, ip_inv, ep, s0, s1, p10, p8, p4)
--- sdes_encryption.py
+++ sdes_encryption.py
@@ -1,13 +1,13 @@
 # sub-keys generation #
-def __permutation(k, p):
+def permutation(text, per):
     p_key = []
-    for i in p:
-        p_key.append(k[i])
+    for i in per:
+        p_key.append(text[i])
 
     return p_key
 
 
-def __rotate_left(k, n):
+def rotate_left(k, n):
     k1 = k[:5]
     k2 = k[5:]
 
@@ -17,14 +17,49 @@ def __rotate_left(k, n):
     return r_k1 + r_k2
 
 
+#
+def xor(x, y):
+    z = [0] * len(x)
+    for i in range(len(x)):
+        if x[i] != y[i]:
+            z[i] = 1
+
+    return z
+
+
+def s_fun(text, s0, s1):
+
+    return []
+
+
+def fk(text, ep, s_key, s0, s1, p4):
+    text_l = text[:4]
+    text_r = text[4:]
+
+    text_ep = permutation(text_r, ep)
+    text_xor = xor(text_ep, s_key)
+    text_s = s_fun(text_xor, s0, s1)
+    text_p4 = permutation(text_s, p4)
+
+    return xor(text_l, text_p4) + text_r
+
+
 # encryption #
-def encryption(plain_text, key, p10, p8):
-    cipher_p10 = __permutation(key, p10)
-    r_cipher_1 = __rotate_left(cipher_p10, 1)
-    r_cipher_2 = __rotate_left(r_cipher_1, 2)
+def encryption(plain_text, key, ip, ip_inv, ep, s0, s1, p10, p8, p4):
+    cipher_p10 = permutation(key, p10)
+    r_cipher_1 = rotate_left(cipher_p10, 1)
+    r_cipher_2 = rotate_left(r_cipher_1, 2)
 
-    sub_key_1 = __permutation(r_cipher_1, p8)
-    sub_key_2 = __permutation(r_cipher_2, p8)
+    sub_key_1 = permutation(r_cipher_1, p8)
+    sub_key_2 = permutation(r_cipher_2, p8)
 
-    cipher_text = ""
+    print(sub_key_1)
+    print(sub_key_2)
+
+    plain_text_ip = permutation(plain_text, ip)
+    first_loop = fk(plain_text_ip, ep, sub_key_1, s0, s1, p4)
+    first_loop_inv = first_loop[4:] + first_loop[:4]
+    second_loop = fk(first_loop_inv, ep, sub_key_2, s0, s1, p4)
+    cipher_text = permutation(second_loop, ip_inv)
+
     return cipher_text
